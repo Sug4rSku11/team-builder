9e15303828e76b6f17667872832b8ed003c77399
desiree <desireegarcia@mini.local>
2021-10-12 16:30:19 -0600

    started Members app

---
+++ src/Members.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Members(props){
+    const {details} = props
+
+    if (!details) {
+        return <h3>Working fetching members...</h3>
+    }
+    return (
+        <div className="members container">
+            <h2>{details.name}</h2>
+            <p>Email:{details.email}</p>
+            <p>Role:{details.role}</p>
+        </div>
+    )
+}
